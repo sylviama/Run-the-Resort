2fc0f8268de5f02ab0c8c8423079b6b7ea026da3
Sylvia Ma <xiangwma@umich.edu>
2016-07-02 19:34:50 -0500

    change map page

--- app/controller/loginCtrl.js
+++ app/controller/loginCtrl.js
@@ -25,13 +25,20 @@ app.controller("loginCtrl",function($scope, authFactory, $location){
       password:$scope.account.password
       //firebase thing:first thing is the error message, second is success function
     },(error, userData)=>{
-    if(error){
-      console.log(`Error creating user: ${error}`)
-    }else{
-      console.log(`Created user account with uid:${userData.uid}`)
-      $scope.login();
-    };
-  });
+      if(error){
+        console.log(`Error creating user: ${error}`);
+      }else{
+        console.log(`Created user account with uid:${userData.uid}`);
+        //login part, except url is /pickMap
+        authFactory.authenticate($scope.account)
+        .then(()=>{
+          $scope.hasUser=true;
+          $scope.userRecord={};
+          $location.url("/pickMap");
+          $scope.$apply();//firebase, make it work...
+        });
+      };
+    })
   };
 
   $scope.login=()=>{
--- app/controller/pickMapCtrl.js
+++ app/controller/pickMapCtrl.js
@@ -1,3 +1,14 @@
-app.controller("pickMapCtrl",function($scope){
-  console.log("pick a map");
+app.controller("pickMapCtrl",function($scope,$location){
+  $scope.confirmMap=function(){
+    $location.url("/record");
+  };
+
+  $scope.cancelChangeMap=function(){
+    $location.url("/record");
+  };
+
+  $scope.selectShowBorder=function(){
+    console.log("picked yellowstone card");
+    $("#yellowstoneCard").attr("class", "clicked card card-size-for-map z-depth-3 hoverable");
+  };
 })
